038cde1809964b823e7b2f25b65fdaa827257ea1
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-11-21 10:17:01 +0100

    Add y translation on gojo to make it look like he moves more

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ const updateParallax = () => {
     const orbPicture = document.querySelector(".orb-picture");
 
     if (scrollPos < 200) {
-        gojoPicture.style.transform = "translate(-50%," + scrollPos + "px)";
-        orbPicture.style.transform = "translateY(" + (scrollPos + (scrollPos/10 * 5)) + "px)";
+        gojoPicture.style.transform = "translate(-50%," + (scrollPos + (scrollPos/10 * 2)) + "px)";
+        orbPicture.style.transform = "translateY(" + (scrollPos + (scrollPos/10 * 8)) + "px)";
         orbPicture.style.transform += "scale(" + (scrollPos/10 + 1) + ")";
     }
 };
