4bc6d2456f9f2cd59a510445ba2cad1e4964c818
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-11-22 00:34:18 +0100

    Add sounds effect on scroll

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+let canPlayAudio = true;
+
 const init = () => {
-    window.onscroll = updateParallax;
+    window.onscroll = () => {
+        playAudio();
+        updateParallax();
+    }
+
+    document.querySelector("#unmute-button").addEventListener("click", unmuteAudioPlayer)
 };
 
 const updateParallax = () => {
     const maxScroll = 200;
     const scrollPos = window.scrollY;
+    if (scrollPos === 0) canPlayAudio = true;
 
     const domainPicture = document.querySelector(".domain-background");
     const gojoPicture = document.querySelector(".gojo-picture");
@@ -18,14 +26,14 @@ const updateParallax = () => {
     const rocksSevenPicture = document.querySelector(".rocks-seven-picture");
 
     if (scrollPos <= maxScroll) {
-        domainPicture.style.width = (scrollPos / 4) + "vmax";
-        domainPicture.style.height = (scrollPos / 4) + "vmax";
-        domainPicture.style.transform = "translate(-50%," + (scrollPos + (scrollPos/10 * 9)) + "px)";
+        domainPicture.style.width = (scrollPos / (maxScroll / 80)) + "vmax";
+        domainPicture.style.height = (scrollPos / (maxScroll / 80)) + "vmax";
+        domainPicture.style.transform = "translate(-50%," + (scrollPos / (maxScroll / 70))/2 + "vmax)";
 
         gojoPicture.style.transform = "translate(-50%," + (scrollPos + (scrollPos/10 * 2)) + "px)";
 
-        //orbPicture.style.transform = "translateY(" + (scrollPos + (scrollPos/10 * 7)) + "px)";
-        //orbPicture.style.transform += "scale(" + (scrollPos/20) + ")";
+        orbPicture.style.transform = "translateY(" + (scrollPos + (scrollPos/10 * 7)) + "px)";
+        orbPicture.style.transform += "scale(" + (scrollPos/20) + ")";
 
         rocksOnePicture.style.transform = "translateY(" + (scrollPos - (scrollPos/10 * 30)) + "px)";
         rocksTwoPicture.style.transform = "translateY(" + (scrollPos - (scrollPos/10 * 19)) + "px)";
@@ -35,8 +43,23 @@ const updateParallax = () => {
         rocksSixPicture.style.transform = "translateY(" + (scrollPos - (scrollPos/10 * 19)) + "px)";
         rocksSevenPicture.style.transform = "translateY(" + (scrollPos - (scrollPos/10 * 28)) + "px)";
     } else {
-        domainPicture.style.width = "50vmax";
-        domainPicture.style.height = "50vmax";
+        domainPicture.style.width = "80vmax";
+        domainPicture.style.height = "80vmax";
     }
 };
+
+const playAudio = () => {
+    const audioPlayer = document.querySelector("#audio-player");
+    if (window.scrollY <= 500) audioPlayer.volume = 0.5 - (window.scrollY / 1000);
+
+    if (audioPlayer.paused && canPlayAudio) {
+        audioPlayer.play();
+        canPlayAudio = false;
+    }
+};
+
+const unmuteAudioPlayer = () => {
+    const audioPlayer = document.querySelector("#audio-player");
+    audioPlayer.muted = !audioPlayer.muted;
+}
 window.onload = init;
