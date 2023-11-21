35c1265e34a3a508e6fa91e358e6e71ea170b834
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-11-21 11:16:01 +0100

    Add rocks to parallax

--- script.js
+++ script.js
@@ -7,11 +7,27 @@ const updateParallax = () => {
 
     const gojoPicture = document.querySelector(".gojo-picture");
     const orbPicture = document.querySelector(".orb-picture");
+    const rocksOnePicture = document.querySelector(".rocks-one-picture");
+    const rocksTwoPicture = document.querySelector(".rocks-two-picture");
+    const rocksThreePicture = document.querySelector(".rocks-three-picture");
+    const rocksFourPicture = document.querySelector(".rocks-four-picture");
+    const rocksFivePicture = document.querySelector(".rocks-five-picture");
+    const rocksSixPicture = document.querySelector(".rocks-six-picture");
+    const rocksSevenPicture = document.querySelector(".rocks-seven-picture");
 
     if (scrollPos < 200) {
         gojoPicture.style.transform = "translate(-50%," + (scrollPos + (scrollPos/10 * 2)) + "px)";
+
         orbPicture.style.transform = "translateY(" + (scrollPos + (scrollPos/10 * 8)) + "px)";
         orbPicture.style.transform += "scale(" + (scrollPos/10 + 1) + ")";
+
+        rocksOnePicture.style.transform = "translateY(" + (scrollPos - (scrollPos/10 * 30)) + "px)";
+        rocksTwoPicture.style.transform = "translateY(" + (scrollPos - (scrollPos/10 * 13)) + "px)";
+        rocksThreePicture.style.transform = "translateY(" + (scrollPos - (scrollPos/10 * 25)) + "px)";
+        rocksFourPicture.style.transform = "translateY(" + (scrollPos - (scrollPos/10 * 20)) + "px)";
+        rocksFivePicture.style.transform = "translateY(" + (scrollPos - (scrollPos/10 * 35)) + "px)";
+        rocksSixPicture.style.transform = "translateY(" + (scrollPos - (scrollPos/10 * 15)) + "px)";
+        rocksSevenPicture.style.transform = "translateY(" + (scrollPos - (scrollPos/10 * 22)) + "px)";
     }
 };
 window.onload = init;
